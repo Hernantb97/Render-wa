415ec4bffc43b0e91e94b5f2a90984ec2fd5652e
hernantb97 <hertenbaigts@hotmail.com>
2025-03-13 17:10:54 -0600

    Agregado webhook para Gupshup

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+  const message = req.body;
+  console.log('Mensaje recibido: ', message);
+  // Aquí puedes agregar el procesamiento para guardar el mensaje en tu base de datos o hacer lo que necesites
+
+  res.status(200).send('Mensaje recibido');
 });
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log('Servidor corriendo en http://localhost:3000');
 });
 
